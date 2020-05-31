de28eb222d678119b6b8a8cdfbd8936b6adea3aa
Bazzastyle <bazzastyle@hotmail.it>
2020-05-31 17:32:20 +0200

    add test tweet

--- tests/routes/utils/index.js
+++ tests/routes/utils/index.js
@@ -1,19 +1,20 @@
 const chai = require('chai');
 const expect = chai.expect;
+const mongoose = require('mongoose');
 
-const User = require('../../../models/user');
+const Tweet = require('../../../models/tweet');
 
 module.exports.expectJson = function (request) {
     expect(request.header).to.has.property('content-type');
     expect(request.header['content-type']).contains('application/json');
 };
 
-module.exports.createUser = async function () {
-    const newUser = {
-        name: 'Steve',
-        surname: 'Jobs',
-        email: 'stevejobs@gmail.com',
-        password: "Pippo"
+module.exports.createTweet = async function () {
+    const newObjectId = mongoose.Types.ObjectId();
+    const newTweet = {
+        _author: newObjectId,
+        tweet: 'Ciao a tutti',
+        created_at: Date.now()
     };
-    return await User.create(newUser);
+    return await Tweet.create(newTweet);
 };
