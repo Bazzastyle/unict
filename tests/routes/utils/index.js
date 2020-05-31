dbf00eef6a72c490177c797e27ab18ca76d2653d
Bazzastyle <bazzastyle@hotmail.it>
2020-05-31 19:23:34 +0200

    fix  module createUser

--- tests/routes/utils/index.js
+++ tests/routes/utils/index.js
@@ -1,5 +1,7 @@
 const chai = require('chai');
 const expect = chai.expect;
+
+const User = require('../../../models/user');
 const mongoose = require('mongoose');
 
 const Tweet = require('../../../models/tweet');
@@ -7,6 +9,16 @@ const Tweet = require('../../../models/tweet');
 module.exports.expectJson = function (request) {
     expect(request.header).to.has.property('content-type');
     expect(request.header['content-type']).contains('application/json');
+};
+
+module.exports.createUser = async function () {
+    const newUser = {
+        name: 'Steve',
+        surname: 'Jobs',
+        email: 'stevejobs@gmail.com',
+        password: "Pippo"
+    };
+    return await User.create(newUser);
 };
 
 module.exports.createTweet = async function () {
